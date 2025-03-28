15719737f7ef91fcf80f63819451a2caf27927a2
maxharrisnet <hello@maxharris.codes>
2025-03-28 13:47:22 -0700

    adds basic files

---
+++ README.md
@@ -0,0 +1,13 @@
+# RevUp Special Offers Plugin
+
+## Developed by: [Max Harris](www.maxharris.net) 👨🏾‍💻
+
+This plugin is written as part of a coding assesment for RevUp. It plots the locations of dental clinics and displays them in a Google map.
+
+### Here's what it does:
+
+## Considerations 🤔
+
+## Future Improvements 📋
+
+Here are some things that I would add to this plugin with more time:
--- revup-clinic-location.php
+++ revup-clinic-location.php
@@ -1,9 +1,15 @@
+<?php
+
 /*
-Plugin Name: RevUp Clinic Location
-Plugin URI: https://github.com/maxharrisnet/revup-clinic-location
-Description: This plugin displays the location of a given dental clinic on a Google map. It creates a shortcode that can be used in posts or pages to show the map.
-Version: 1.0
-Author: Max Harris for RevUp Dental
-Author URI: https://www.maxharris.net
-Text Domain: revup
+  Plugin Name: RevUp Clinic Location
+  Plugin URI: https://github.com/maxharrisnet/revup-clinic-location
+  Description: This plugin displays the location of a given dental clinic on a Google map. It creates a shortcode that can be used in posts or pages to show the map.
+  Version: 1.0
+  Author: Max Harris for RevUp Dental
+  Author URI: https://www.maxharris.net
+  Text Domain: revup
 */
+
+if (! defined('ABSPATH')) {
+  exit; // Exit if accessed directly
+}

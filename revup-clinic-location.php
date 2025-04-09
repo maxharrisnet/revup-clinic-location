269ff0bc149d3385f64e1e83c1e117eeb4c86813
maxharrisnet <hello@maxharris.codes>
2025-04-09 11:55:28 -0700

    version 1.01

--- revup-clinic-location.php
+++ revup-clinic-location.php
@@ -4,7 +4,7 @@
   Plugin Name: RevUp Clinic Location
   Plugin URI: https://github.com/maxharrisnet/revup-clinic-location
   Description: This plugin displays the location of a given dental clinic on a Google map. It creates a shortcode that can be used in posts or pages to show the map.
-  Version: 1.0
+  Version: 1.01
   Author: Max Harris for RevUp Dental
   Author URI: https://www.maxharris.net
   Text Domain: revup
